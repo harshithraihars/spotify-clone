07aa116a07a11290c8d9cde0f7e946f4bb34c5ab
unknown <harshithraiharsu@gmail.com>
2024-10-12 15:28:26 +0530

    last

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // base:"/spotify-clone/",
+// })
 export default defineConfig({
   plugins: [react()],
-  // base:"/spotify-clone/",
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: To split vendor code into a separate chunk
+          vendor: ['react', 'react-router-dom'],
+        },
+      },
+    },
+  },
+});
