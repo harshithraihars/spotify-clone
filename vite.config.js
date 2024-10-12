93a21d2313fae5081585cfe9796f3ce7ad476329
unknown <harshithraiharsu@gmail.com>
2024-10-12 15:31:11 +0530

    last

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@
 //   plugins: [react()],
 //   // base:"/spotify-clone/",
 // })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
